179946cfe35f7e4cd0993a98bcdee6ab946683f9
samuraifox1 <rjs.swarnkar@outlook.com>
2025-09-25 19:52:00 +0530

    minor update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # times-of-india-textanalytics
 Analysing Time Of India News Articles and Comments using using Azure AI Language Service
+
+## About: 
+
+## Reference: 
+
+* [AI Language Service](https://microsoftlearning.github.io/mslearn-ai-language/)
--- deploy-azure-aiservice/run.cmd
+++ deploy-azure-aiservice/run.cmd
@@ -2,8 +2,7 @@
 
 az account clear
 az login
-az account set --subscription "Your Subscription Name or ID"
-
+# az account set --subscription "Your Subscription Name or ID"
 az account set --subscription  
 az account show
 
@@ -14,3 +13,5 @@ terraform apply -auto-approve
 
 terraform plan -destroy
 terraform destroy -auto-approve
+
+az account clear
